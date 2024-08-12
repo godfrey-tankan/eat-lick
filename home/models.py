e6bf59ebc44f3caebd3421b25c297a45cf6b66a5
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-12 14:51:23 +0200

    Refactor handle_help function in views.py and optimize code for better performance

--- home/models.py
+++ home/models.py
@@ -18,7 +18,7 @@ class Ticket(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     created_by = models.CharField(max_length=255, null=True, blank=True)
-    assigned_to = models.CharField(max_length=255, null=True, blank=True)
+    assigned_to = models.ForeignKey('SupportMember', related_name='assigned_tickets', on_delete=models.SET_NULL, null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='open')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -26,13 +26,9 @@ class Ticket(models.Model):
     closed_at = models.DateTimeField(null=True, blank=True)
     expired_at = models.DateTimeField(null=True, blank=True)
 
-    # def save(self, *args, **kwargs):
-    #     if not self.assigned_to:
-    #         admin_user = User.objects.filter(is_staff=True).first() 
-    #         self.assigned_to = admin_user
-    #     super().save(*args, **kwargs)
     def __str__(self):
         return f"Ticket #{self.id} - {self.title} ({self.status})"
+
 
 class TicketLog(models.Model):
     ticket = models.ForeignKey(Ticket, related_name='logs', on_delete=models.CASCADE)
--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import Ticket, TicketLog, Comment, FAQ
+from .models import *
 from .serializers import TicketSerializer, TicketLogSerializer, CommentSerializer, FAQSerializer
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import TicketForm, CommentForm
@@ -14,9 +14,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import DeleteView
 from django.urls import reverse_lazy
 from .models import Ticket
+from django.db.models import Count, Q
 
-
-# Create your views here.
 @login_required
 def index(request):
     # Get all tickets
@@ -29,7 +28,18 @@ def index(request):
     resolved_tickets_count = all_tickets.filter(status='resolved').count()
 
     def calculate_percentage(count):
-        return round((count / total_tickets * 100),2) if total_tickets > 0 else 0
+        return round((count / total_tickets * 100), 2) if total_tickets > 0 else 0
+
+    # Get support members who resolved more than 5 tickets
+    active_support_members = SupportMember.objects.filter(
+        username__in=Ticket.objects.filter(
+            status='resolved'
+        ).values('assigned_to')
+    ).annotate(
+        resolved_tickets_count=Count('username')
+    ).filter(
+        resolved_tickets_count__gt=1
+    )
 
     context = {
         "tickets_count": total_tickets,
@@ -41,9 +51,11 @@ def index(request):
         "closed_tickets_percentage": calculate_percentage(closed_tickets_count),
         "pending_tickets_percentage": calculate_percentage(pending_tickets_count),
         "resolved_tickets_percentage": calculate_percentage(resolved_tickets_count),
+        "active_support_members": active_support_members or 0,
     }
 
     return render(request, 'pages/index.html', context=context)
+
 
 # Create your views here.
 def home_view(request):
