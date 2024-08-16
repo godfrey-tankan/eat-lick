cb4f6b2bdbf593236d6a2efbb443ea8469e4243c
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-16 10:27:13 +0200

    Update global search functionality to include user search and ticket search

---
+++ home/global_search.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.shortcuts import render
+from .models import Ticket
+
+def global_search(request):
+    query = request.GET.get('q', '')
+    users = tickets = None
+    
+    if query:
+        users = User.objects.filter(
+            Q(username__icontains=query) | 
+            Q(email__icontains=query) |
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query)
+        )
+        tickets = Ticket.objects.filter(
+            Q(description__icontains=query) |
+            Q(status__icontains=query) |
+            Q(created_by__username__icontains=query) |
+            Q(assigned_to__username__icontains=query)
+        )
+    
+    context = {
+        'users': users,
+        'tickets': tickets,
+        'query': query,
+    }
+    return render(request, 'search_results.html', context)
