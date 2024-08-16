298afa89e58850fe337def95be15740b7402038c
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-16 10:49:03 +0200

    Refactor global_search.py to include user search for non-superusers

--- home/global_search.py
+++ home/global_search.py
@@ -6,21 +6,29 @@ from .models import Ticket
 def global_search(request):
     query = request.GET.get('q', '')
     users = tickets = None
-    
     if query:
-        users = User.objects.filter(
-            Q(username__icontains=query) | 
-            Q(email__icontains=query) |
-            Q(first_name__icontains=query) |
-            Q(last_name__icontains=query)
-        )
-        tickets = Ticket.objects.filter(
-            Q(description__icontains=query) |
-            Q(status__icontains=query) |
-            Q(created_by__username__icontains=query) |
-            Q(assigned_to__username__icontains=query)
-        )
-    
+        if request.user.is_superuser:
+            users = User.objects.filter(
+                Q(username__icontains=query) | 
+                Q(email__icontains=query) |
+                Q(first_name__icontains=query) |
+                Q(last_name__icontains=query)
+            )
+            tickets = Ticket.objects.filter(
+                Q(description__icontains=query) |
+                Q(status__icontains=query) |
+                Q(created_by__username__icontains=query) |
+                Q(assigned_to__username__icontains=query)
+            )
+        else:
+            users = None
+            tickets = Ticket.objects.filter(
+                Q(description__icontains=query) |
+                Q(status__icontains=query) |
+                Q(created_by__username__icontains=query) |
+                Q(assigned_to__username__iexact=request.user.id)
+            )
+
     context = {
         'users': users,
         'tickets': tickets,
