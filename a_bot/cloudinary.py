4acb1c5636a5bbb31a74aaec92288caf92879239
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-18 00:11:03 +0200

    Refactor cloudinary configuration to comment out unnecessary code

--- a_bot/cloudinary.py
+++ a_bot/cloudinary.py
@@ -1,7 +1,7 @@
-import cloudinary
+# import cloudinary
           
-cloudinary.config( 
-  cloud_name = "dhhcmql4h", 
-  api_key = "445864248719783", 
-  api_secret = "BmXUBOH_JfeEdz4UR6PXGR6lEdk" 
-)
+# cloudinary.config( 
+#   cloud_name = "dhhcmql4h", 
+#   api_key = "445864248719783", 
+#   api_secret = "BmXUBOH_JfeEdz4UR6PXGR6lEdk" 
+# )
