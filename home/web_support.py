b933cbfde6b4ea3ec0ae4b9b0db6db626140352e
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-18 15:41:02 +0200

    Refactor web support page to handle user messages and improve functionality

--- home/web_support.py
+++ home/web_support.py
@@ -10,13 +10,29 @@ from .forms import *
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .helpers import format_phone_number
+from a_bot.responses import *
 
 @csrf_exempt
 def web_support(request):
     if request.method == 'POST':
         data = json.loads(request.body)
+        print(data)
         phone = data.get('phone')
         phone = format_phone_number(phone)
+        try:
+            get_inquirer = Inquirer.objects.get(phone_number=phone)
+        except Inquirer.DoesNotExist:
+            get_inquirer = None
+        if get_inquirer:
+            if get_inquirer.user_mode == NAMES_MODE:
+                # get_inquirer.username = data.get('names')
+                # get_inquirer.user_mode = 'ticket_creation'
+                # get_inquirer.save()
+                return JsonResponse({'success': True, 'message': f'Hello we testing your names'})
+            return JsonResponse({'success': True, 'message': f'Hello {get_inquirer.username}, how can we help you today?.'})
+        else:
+            Inquirer.objects.create(phone_number=phone,user_mode =NAMES_MODE)
+            return JsonResponse({'success': True, 'message': f'We are creating an account for you. Please Please provide your names'})
         if not phone:
             return JsonResponse({'success': False, 'error': 'Phone number is required.'})
         return JsonResponse({'success': True, 'message': f'Welcome! Your phone number {phone} has been registered.'})
