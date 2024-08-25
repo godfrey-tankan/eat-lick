db83c76922ee7926f0b5ec1c104c64a431db6ab4
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-25 12:42:53 +0200

    `Refactor views.py to fix response handling in generate_response`

--- a_bot/views.py
+++ a_bot/views.py
@@ -329,7 +329,7 @@ def get_attended_tickets(support_member,response):
         message = 'Tickets being attended:\n\n'
         for i,ticket in enumerate(attended_tickets,start=1):
             created =timezone.localtime(ticket.created_at).strftime('%Y-%m-%d %H:%M')
-            message += f"*{i}*. Ticket Number: *{ticket.id}- *Attended by *{ticket.assigned_to.username}*\nOpened by: *{ticket.created_by.username.title()}* from *{ticket.branch_opened.title()}* branch at {created}\n- Description: {ticket.description}\n\n"
+            message += f"*{i}*. Ticket Number: *{ticket.id}- *Attended by *{ticket.assigned_to.username}*\n- Opened by: *{ticket.created_by.username.title()}* from *{ticket.branch_opened.title()}* branch at {created}\n- Description: {ticket.description}\n\n"
         message += '\nReply with *#exit* to exit'
         support_member.user_status = ATTENDED_TICKETS_MODE
         support_member.save()
