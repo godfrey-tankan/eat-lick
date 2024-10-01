53eab1d34f8f3f6b1577d009904b131177c488ea
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-01 11:58:51 +0200

    Refactor database configuration in settings.py

--- home/forms.py
+++ home/forms.py
@@ -18,6 +18,7 @@ class SupportMemberForm(forms.ModelForm):
     class Meta:
         model = SupportMember
         fields = ['username', 'phone_number', 'is_active'] 
+        
 
 class UserForm(forms.ModelForm):
     class Meta:
