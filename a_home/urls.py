1621e77970228f0d104400dbb388f28db1b9c245
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-22 16:58:30 +0200

    Refactor URL path for staff dashboard and add feedback details page
    - Updated the URL path for the staff dashboard in a_home/urls.py
    - Added a new URL path for the feedback details page in a_home/urls.py
    - Modified the showPopup function in feedback_detail.html to fetch the feedback details from the new URL path
    
    Refactor code structure and add necessary files for SMEDCO module
    
    - Restructured the code in a_home/views.py to include a new function called test
    - Added necessary files for the SMEDCO module
    
    Refactor generate_response function to include open inquiries count and option to view open tickets
    
    - Modified the generate_response function in a_home/views.py to include the count of open inquiries
    - Added an option to view open tickets in the generate_response function
    
    Refactor greeting messages to include "hy" as a greeting option
    
    - Updated the greeting messages in a_home/views.py to include "hy" as a greeting option
    
    Refactor database configuration in settings.py
    
    - Modified the database configuration in core/settings.py to use SQLite3 instead of MySQL

--- a_home/urls.py
+++ a_home/urls.py
@@ -4,14 +4,14 @@ from a_home.views import *
 urlpatterns = [
     path('', home_view, name='home'),
     path('start/', demographic_data_view, name='start'),
+    path('feedback/<int:user_id>/details/', feedback_details, name='detailed_feedback_page'),
     path('thank-you/', thank_you, name='thank_you'),
     path('job-satisfaction/', job_satisfaction_view, name='job_satisfaction'),
     path('aggregated-feedback/', aggregated_feedback_view, name='aggregated_feedback'),
     path('smedco/staff/', staff_dashboard_view, name='staff_dashboard'),
     path('feedbacks/', feedback_list, name='feedback_list'),
     path('feedbacks/details/', feedback_detail, name='feedback_detail'),
-    path('feedback/<int:user_id>/details/', feedback_details, name='detailed_feedback_page'),
-     path('feedback/<int:user_id>/details/', get_user_feedback, name='user_feedback_details'),
+    #  path('feedback/<int:user_id>/details/', get_user_feedback, name='user_feedback_details'),
     # path('members/',test,name='members')
     
 ]
